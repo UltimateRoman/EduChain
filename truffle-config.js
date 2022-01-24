afeba21615fc0008fa4f280f2892738659b6d92a
UltimateRoman <59477486+UltimateRoman@users.noreply.github.com>
2022-01-24 18:31:40 +0530

    Initial Commit

---
+++ truffle-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    }
+  },
+
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis/',
+  compilers: {
+    solc: {
+      version: "0.7.1",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
