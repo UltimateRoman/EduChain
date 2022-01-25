601671152622d4074d3c8adda71dfb506438b03f
UltimateRoman <59477486+UltimateRoman@users.noreply.github.com>
2022-01-25 13:19:23 +0530

    Added web3 methods

--- src/components/App.js
+++ src/components/App.js
@@ -15,11 +15,17 @@ class App extends Component {
     if (window.celo) {
       await window.celo.enable();
       window.web3 = new Web3(window.celo);
-    }
-    else if (window.web3) {
+      const networkId = await window.web3.eth.net.getId();
+      if (networkId === 44787) {
+        this.setState({ network: "Alfajores" });
+      }
+    } else if (window.web3) {
       window.web3 = new Web3(window.web3.currentProvider);
-    }
-    else {
+      const networkId = await window.web3.eth.net.getId();
+      if (networkId === 44787) {
+        this.setState({ network: "Alfajores" });
+      }
+    } else {
       window.alert('Use the Celo Extension Wallet!');
     }
   }
@@ -28,25 +34,86 @@ class App extends Component {
     const web3 = window.web3;
     const accounts = await web3.eth.getAccounts();
     this.setState({ account: accounts[0] });
+    const networkId = await web3.eth.net.getId();
+    const networkData = EduchainMain .networks[networkId];
+
+    if (networkData) {
+      const ECMain = new web3.eth.Contract(EduchainMain.abi, networkData.address);
+      this.setState({ ECMain });
+      const cCount = await ECMain.methods.contentCount().call();
+      for (let i = 0; i < cCount; ++i) {
+        const content = await ECMain.methods.contents(i).call();
+        this.setState({
+          contents: [...this.state.contents, content]
+        });
+      }
+    } else {
+      window.alert('The current network is not supported, use Celo Alfajores Testnet');
+    }
     this.setState({ loading: false });
+  }
+
+  async getContributor(file) {
+    this.setState({ loading: true });
+    const contributor = await ECMain.methods.getContributor(file).call();
+    this.setState({ loading: false });
+    return contributor;
+  }
+
+  tipContributor(contributor, amount) {
+    this.setState({ loading: true });
+    window.web3.eth.sendTransaction({ 
+      from: this.state.account, 
+      to: contributor,
+      value: window.web3.utils.toWei(amount.toString())
+    })
+    .once('confirmation', (n, receipt) => {
+      this.setState({ loading: false });
+      window.location.reload();
+      window.alert('Successfully funded '+contributor);
+    });
+    this.setState({ loading: false });
+  }
+
+  addContent(course, subject, file) {
+    this.setState({ loading: true });
+    this.state.ECMain.methods.addContent(course, subject, file)
+    .send({ from: this.state.account })
+    .once('confirmation', (n, receipt) => {
+      this.setState({ loading: false });
+      window.location.href = '/resources';
+    });
   }
 
   constructor(props) {
     super(props);
     this.state = {
       account: '',
-      loading: false
+      loading: false,
+      ECMain: null,
+      contents: [],
+      network: "Unidentified"
     }
+
+    this.addContent = this.addContent.bind(this);
   }
 
   render() {
-    return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <h1>EduChain</h1>
-        </nav>
-      </div>
-    );
+    if (this.state.network !== "Unidentified") {
+      return (
+        <div style={{ height: 800 }}>
+          <Router>
+            <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+              <h1>EduChain</h1>
+            </nav>
+          </Router>
+        </div>
+      );
+    } else {
+      return(
+        <p style={{textAlign: "center", fontSize: "30 px"}}>Your current browser is not supported. Install Celo Extension wallet and use Celo Alfajores Testnet.</p>
+      );
+    }
   }
 }
 
